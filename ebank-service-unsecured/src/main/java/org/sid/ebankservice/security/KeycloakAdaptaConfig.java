55fb64d6394f39d1943c84f8aa36ad016ee02152
Mohamed Lamine KONE <mlkone@Ibrahims-MacBook-Air-2.local>
2022-12-09 10:41:36 +0000

    first commit

---
+++ ebank-service-unsecured/src/main/java/org/sid/ebankservice/security/KeycloakAdaptaConfig.java
@@ -0,0 +1,14 @@
+package org.sid.ebankservice.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //pour dire que la configuration de keycloack se trouve dans l'application properties
+public class KeycloakAdaptaConfig {
+
+    @Bean
+    KeycloakSpringBootConfigResolver springBootConfigResolver(){
+        return  new KeycloakSpringBootConfigResolver();
+    }
+}
---
+++ ebank-service-unsecured/src/main/java/org/sid/ebankservice/security/KeycloakSecurityConfig.java
@@ -0,0 +1,36 @@
+package org.sid.ebankservice.security;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    //cette methode va permettre a keycloak de  chercher ou trouver les utilisateurs
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider()) ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
+        //autoriser l'acces au console de h2 sans authentification
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.headers().frameOptions().disable(); //permettre a spring security d'afficher les frames de h2 database
+        //proteger tous nos requettes avec une authentification
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
